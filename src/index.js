9ce6fd5e436e878fad8b7ceba0ebe90fe4a0048f
Ievgennii <simonovievgenii@gmail.com>
2023-06-15 23:09:12 +0300

    remove Persist

--- src/index.js
+++ src/index.js
@@ -3,20 +3,20 @@ import ReactDOM from 'react-dom/client';
 import App from 'components/App';
 import './index.css';
 import { Provider } from 'react-redux';
-import { persistR, store } from 'redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
+import {  store } from 'redux/store';
+// import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistR}>
+      
         <App />
-      </PersistGate>
+      
     </Provider>
   </React.StrictMode>
 );
 
 
-//<Provider store={store}> ... </Provider>: Оборачивает <App /> в <Provider>. 
+
 //<Provider> предоставляет доступ к Redux store для всех компонентов, которые находятся внутри него. 
 //Здесь Redux store передается в качестве свойства store
