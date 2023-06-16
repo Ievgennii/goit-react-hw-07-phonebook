f5ab7b8611e29f2c686191743164010792d0b357
Ievgennii <simonovievgenii@gmail.com>
2023-06-16 20:50:26 +0300

    add thunks

--- src/redux/sliceContact.js
+++ src/redux/sliceContact.js
@@ -1,16 +1,23 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit';
 
 export const sliceContact = createSlice({
-  name: 'contacts', // Название slice, будет использовано в генерации actions и reducer'а
-  initialState: [
-    { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' }, // Начальное состояние контактов
-    { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
-    { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
-    { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
-  ],
+  name: 'contacts',
+  initialState: {
+    items: [
+      {
+        createdAt: '2023-04-05T23:25:30.677Z',
+        name: 'Dolores Morar',
+        phone: '(570) 817-2230',
+        id: '29',
+      },
+    ],
+    isLoading: false,
+    error: null,
+  },
   reducers: {
     add(state, action) {
-      state.push({ // Добавление нового контакта в состояние
+      state.push({
+        // Добавление нового контакта в состояние
         id: nanoid(),
         name: action.payload.name,
         number: action.payload.number,
@@ -25,13 +32,12 @@ export const sliceContact = createSlice({
 
 export const { add, remove } = sliceContact.actions; // Экспорт сгенерированных actions
 
-
-// Этот код использует функцию createSlice из Redux Toolkit для создания slice (части состояния) с названием "contacts". 
+// Этот код использует функцию createSlice из Redux Toolkit для создания slice (части состояния) с названием "contacts".
 // Slice содержит начальное состояние initialState, которое представляет собой массив объектов контактов.
 // reducers объект содержит определения для двух actions: add и remove.
-// add action добавляет новый контакт в состояние. Он получает name и number из action.payload и 
+// add action добавляет новый контакт в состояние. Он получает name и number из action.payload и
 // добавляет новый объект контакта с уникальным id с помощью функции nanoid().
-// remove action удаляет контакт из состояния. Он использует action.payload (идентификатор контакта) для фильтрации 
+// remove action удаляет контакт из состояния. Он использует action.payload (идентификатор контакта) для фильтрации
 // состояния и возвращает новый массив контактов, исключая удаляемый контакт.
-// sliceContact.actions экспортирует сгенерированные actions add и remove, которые могут быть использованы 
+// sliceContact.actions экспортирует сгенерированные actions add и remove, которые могут быть использованы
 // для диспетчеризации и обновления состояния контактов в Redux store
